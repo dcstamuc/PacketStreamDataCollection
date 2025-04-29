d9857ae33d246d95df63f28506ad41e9e2fd2a2c
Disruptive Cyber-Security Lab at TAMU-Commerce <38592881+dcstamuc@users.noreply.github.com>
2025-04-28 20:21:57 -0500

    Delete Packet_stream_attack_identification-main directory

--- Packet_stream_attack_identification-main/pretrain.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python pretrain.py -s 0 -k 3 --n_streams 10000 -i data/0901_0930_K_3.csv --hidden_dims 20 --epochs 10000 --context_dims 3 --lr 0.001 --batch_size 20 --n_layers 3 -r pre_results --exp_str 0_hid20_context3_k3_s0 --label_config config_label.yaml  
--- Packet_stream_attack_identification-main/train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python main.py -s 0 -k 3 --n_streams 10000 -i data/0901_0930_K_3.csv --model_type ann --hidden_dims 20 --epochs 1000 --context_dims 0 --lr 0.001 --batch_size 20 --n_layers 3 -r results --exp_str 0_ann_hid20_k3_s0 --label_config config_label.yaml
-
-python main.py -s 0 -k 3 --n_streams 10000 -i data/0901_0930_K_3.csv --model_type lstm --hidden_dims 20 --epochs 1000 --context_dims 3 --lr 0.001 --batch_size 20 --n_layers 3 -r results --exp_str 0_lstm_hid20_context3_k3_s0 --label_config config_label.yaml
-
-python main.py -s 0 -k 3 --n_streams 10000 -i data/0901_0930_K_3.csv --model_type bi_lstm --hidden_dims 20 --epochs 1000 --context_dims 3 --lr 0.001 --batch_size 20 --n_layers 3 -r results --exp_str 0_bi_lstm_hid20_context3_k3_s0 --label_config config_label.yaml
-
-python main.py -s 0 -k 3 --n_streams 10000 -i data/0901_0930_K_3.csv --model_type seq2seq --hidden_dims 20 --epochs 1000 --context_dims 3 --lr 0.001 --batch_size 20 --n_layers 3 -r results --exp_str 0_seq2seq_hid20_context3_k3_s0 --pre_model_path pre_results/0_hid20_context3_k3_s0/best_model.pt --label_config config_label.yaml
